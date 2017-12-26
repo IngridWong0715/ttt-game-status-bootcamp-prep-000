20d9b5ef83b83bbba370f46096ef8cc78d50455c
Ingrid Wong <yw340@georgetown.edu>
2017-12-26 23:58:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,10 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combination|
-    if combination.all? {|index| board[index] == "X" || board[index] == "O"}
+    if combination.all? {|index| board[index] == "X"}
       return combination
     elsif combination.all? {|index| board[index] == "O"}
+      return combination
 
     end
     puts combination
